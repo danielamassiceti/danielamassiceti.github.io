23923399e1cb78804827f89601937411f5970c22
Daniela Massiceti <daniela.massiceti@gmail.com>
2021-07-29 21:22:53 +0100

    fixed index and about

---
+++ global_vars.js
@@ -0,0 +1,3 @@
+var cv = 'danielamassiceti_cv_may2020.pdf';
+var mykeywords = 'daniela massiceti, university of oxford, phd student, phil torr, torr vision group, computer vision, oxford, microsoft research, cambridge, orbit';
+var jobtitle = 'Senior Researcher, Microsoft Research Cambridge';
